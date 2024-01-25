69f79d30dd41ee3e68b004f4f4f327ef1037128c
RodicaNegura <RodicaElenaNegura@gmail.com>
2024-01-25 09:50:00 +0000

    added image file

--- day1-photo_slider/src/App.js
+++ day1-photo_slider/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+ import car1 from "./assets/img/car-1.jpg";
+ import car2 from "./assets/img/car-2.jpg";
+ import car3 from "./assets/img/car-3.jpg";
+ import car4 from "./assets/img/car-4.jpg";
+ import car5 from "./assets/img/car-5.jpg";
+
+const IMAGES = [car1, car2, car3, car4, car5]
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  return <ImageSlider imageUrls={IMAGES}/> 
 }
 
 export default App;
